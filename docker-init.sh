ccd7051507cba2216295c8f5e70fcacd19ba542d
Jan Macku <jamacku@redhat.com>
2019-02-13 20:59:42 +0100

    Add source for libmd

--- docker-init.sh
+++ docker-init.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
 apt update
-apt -y install wget
+apt -y install wget vim
 
 cd /home
-# wget for libmd0
-#wget 
 
-wget https://launchpad.net/ubuntu/+archive/primary/+files/mtree-netbsd_20180822-4_amd64.deb && wget https://launchpad.net/ubuntu/+archive/primary/+files/mtree-netbsd-dbgsym_20180822-4_amd64.ddeb
+# libmd0
+wget https://launchpad.net/ubuntu/+archive/primary/+files/libmd0_1.0.1-1_amd64.deb
+wget https://launchpad.net/ubuntu/+archive/primary/+files/libmd0-dbgsym_1.0.1-1_amd64.ddeb
+wget https://launchpad.net/ubuntu/+archive/primary/+files/libmd-dev_1.0.1-1_amd64.deb
+
+dpkg -i ./libmd0_1.0.1-1_amd64.deb ./libmd0-dbgsym_1.0.1-1_amd64.ddeb ./libmd-dev_1.0.1-1_amd64.deb
+
+# libc6
+# wget https://launchpad.net/ubuntu/+archive/primary/+files/libc6-dbg_2.28-0ubuntu1_amd64.deb
+# wget https://launchpad.net/ubuntu/+archive/primary/+files/libc6-amd64-dbgsym_2.28-0ubuntu1_i386.ddeb
+# wget https://launchpad.net/ubuntu/+archive/primary/+files/libc6-dev_2.28-0ubuntu1_amd64.deb
+
+# mtree-netbsd
+wget https://launchpad.net/ubuntu/+archive/primary/+files/mtree-netbsd_20180822-4_amd64.deb
+wget https://launchpad.net/ubuntu/+archive/primary/+files/mtree-netbsd-dbgsym_20180822-4_amd64.ddeb
+
 dpkg -i ./mtree-netbsd_20180822-4_amd64.deb ./mtree-netbsd-dbgsym_20180822-4_amd64.ddeb
 
 
